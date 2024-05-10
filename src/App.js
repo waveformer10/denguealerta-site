b3bec84ee1788e75bc08e794faf218148ab92556
Marcelo <waveformer10@gmail.com>
2024-05-10 18:32:32 -0300

    inclusao de Mapa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Layout from "./components/Layout";
 import Lateral from "./components/Lateral";
 import MessageData from "./components/MessageData";
+import "./App.css"
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 function App() {
   return (
@@ -14,8 +16,18 @@ function App() {
             <MessageData text={'Hello world 2'} newInfo={false} />
           </nav>
         </>
-      }>
-        <p>Mapa</p>
+        }>
+        <MapContainer center={[-18.57500282606707, -46.514107349614726]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[-18.57500282606707, -46.514107349614726]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </Layout>
     </div>
   );
